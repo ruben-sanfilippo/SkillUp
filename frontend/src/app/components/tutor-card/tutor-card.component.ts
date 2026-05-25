ad2c3070a02d2d8cc11906b78f310f004c851c5d
Salvatore Scaravalle <salvo.scaravalle@gmail.com>
2026-05-25 18:01:55 +0200

    feat: implement tutor card component with dynamic data; enhance search tutor page with filters and improved layout

--- frontend/src/app/components/tutor-card/tutor-card.component.ts
+++ frontend/src/app/components/tutor-card/tutor-card.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { addIcons } from 'ionicons';
+import { star } from 'ionicons/icons';
+
+import { IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tutor-card',
   templateUrl: './tutor-card.component.html',
   styleUrls: ['./tutor-card.component.scss'],
+  standalone: true,
+  imports: [CommonModule, IonIcon],
 })
-export class TutorCardComponent  implements OnInit {
+export class TutorCardComponent {
+  @Input() tutor: any;
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  constructor() {
+    addIcons({ star });
+  }
 }
--- frontend/src/app/pages/search-tutor/search-tutor.page.ts
+++ frontend/src/app/pages/search-tutor/search-tutor.page.ts
@@ -1,20 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { searchOutline } from 'ionicons/icons';
+import { TutorCardComponent } from '../../components/tutor-card/tutor-card.component';
+//Devo aggiungere in disponibilita Da giorno x a giorno y e levare le checkbox
+
+import {
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonIcon,
+  IonRange,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-search-tutor',
   templateUrl: './search-tutor.page.html',
   styleUrls: ['./search-tutor.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+
+  imports: [
+    CommonModule,
+    FormsModule,
+    TutorCardComponent,
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonIcon,
+    IonRange,
+  ],
 })
 export class SearchTutorPage implements OnInit {
+  tutors = [
+    {
+      name: 'Dr.ssa Elena Rostova',
+      subject: 'Matematica',
+      bio: 'Dottorato in Matematica Applicata. Esperta in Analisi Matematica e Algebra.',
+      rating: 4.9,
+      reviews: 124,
+      price: 45,
+      image: 'https://i.pravatar.cc/150?img=1',
+    },
+    {
+      name: 'Marco Chen',
+      subject: 'Fisica',
+      bio: 'Ex ingegnere aerospaziale diventato un appassionato tutor di fisica.',
+      rating: 5.0,
+      reviews: 89,
+      price: 60,
+      image: 'https://i.pravatar.cc/150?img=11',
+    },
+    {
+      name: 'Sara Jenkins',
+      subject: 'Letteratura',
+      bio: 'Appassionata di scrittura di saggi, letteratura moderna e poesia.',
+      rating: 4.8,
+      reviews: 210,
+      price: 35,
+      image: 'https://i.pravatar.cc/150?img=5',
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ searchOutline });
   }
 
+  ngOnInit() {}
 }
