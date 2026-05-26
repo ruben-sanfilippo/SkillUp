01e2859f2b9c5c31f89d44305d155e82e920522c
ruben-sanfilippo <rubensanfilippo2@gmail.com>
2026-05-26 18:29:50 +0200

    TUTOR PROFILE AGGIORNATA, MANCA SOLO CALENDARIO

--- frontend/src/app/pages/tutor-profile/tutor-profile.page.ts
+++ frontend/src/app/pages/tutor-profile/tutor-profile.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { addIcons } from 'ionicons';
 import {
   personOutline,
@@ -24,12 +25,22 @@ import {
   cloudUploadOutline,
   addCircleOutline,
   documentOutline,
+  eyeOutline,
+  checkmarkCircleOutline,
+  closeOutline,
+  downloadOutline,
 } from 'ionicons/icons';
 
 interface Dispensa {
   titolo: string;
   descrizione: string;
   prezzo: number | null;
+  copertinaUrl?: string;
+  anteprimaUrl?: string | SafeResourceUrl;
+  isPdfPreview?: boolean;
+  fileCompleto?: File | null;
+  haAnteprima: boolean;
+  haFileCompleto: boolean;
 }
 
 @Component({
@@ -51,19 +62,18 @@ export class TutorProfilePage implements OnInit {
   materieSelezionate: string[] = ['Matematica', 'Fisica'];
   lingueSelezionate: string[] = ['Italiano'];
 
-  // Variabili di stato "Editing" separate
   isEditingBiografia = false;
   isEditingLingue = false;
   isEditingMaterie = false;
   isEditingDisponibilita = false;
+  isViewingAnteprima = false;
 
-  // Buffer Temporanei
   biografiaTmp = '';
   prezzoOrarioTmp = 0;
   materieSelezionateTmp: string[] = [];
   lingueSelezionateTmp: string[] = [];
+  dispensaInEvidenza: Dispensa | null = null;
 
-  // Pool per autocompletamento
   materieDisponibili = [
     'Matematica',
     'Fisica',
@@ -85,10 +95,22 @@ export class TutorProfilePage implements OnInit {
   lingueFiltrate: string[] = [];
   mostraListaLingue = false;
 
-  nuovaDispensa: Dispensa = { titolo: '', descrizione: '', prezzo: null };
+  avatarUrl = '';
+
+  nuovaDispensa: Dispensa = {
+    titolo: '',
+    descrizione: '',
+    prezzo: null,
+    copertinaUrl: '',
+    anteprimaUrl: '',
+    fileCompleto: null,
+    haAnteprima: false,
+    haFileCompleto: false,
+    isPdfPreview: false,
+  };
   listaDispense: Dispensa[] = [];
 
-  constructor() {
+  constructor(private sanitizer: DomSanitizer) {
     addIcons({
       personOutline,
       cameraOutline,
@@ -110,6 +132,10 @@ export class TutorProfilePage implements OnInit {
       cloudUploadOutline,
       addCircleOutline,
       documentOutline,
+      eyeOutline,
+      checkmarkCircleOutline,
+      closeOutline,
+      downloadOutline,
     });
   }
 
@@ -124,7 +150,6 @@ export class TutorProfilePage implements OnInit {
     this.lingueSelezionateTmp = [...this.lingueSelezionate];
     this.isEditingLingue = true;
   }
-
   attivaModificaMaterie() {
     this.materieSelezionateTmp = [...this.materieSelezionate];
     this.isEditingMaterie = true;
@@ -149,19 +174,16 @@ export class TutorProfilePage implements OnInit {
       this.materieSelezionateTmp.push(m);
     this.mostraListaMaterie = false;
   }
-
   rimuoviMateria(m: string) {
     this.materieSelezionateTmp = this.materieSelezionateTmp.filter(
       (item) => item !== m,
     );
   }
-
   aggiungiLingua(l: string) {
     if (!this.lingueSelezionateTmp.includes(l))
       this.lingueSelezionateTmp.push(l);
     this.mostraListaLingue = false;
   }
-
   rimuoviLingua(l: string) {
     this.lingueSelezionateTmp = this.lingueSelezionateTmp.filter(
       (item) => item !== l,
@@ -170,7 +192,9 @@ export class TutorProfilePage implements OnInit {
 
   salvaSezione(sezione: string) {
     if (sezione === 'biografia') {
-      this.biografia = this.biografiaTmp;
+      this.biografia = this.biografiaTmp
+        ? this.biografiaTmp.substring(0, 200)
+        : '';
       this.isEditingBiografia = false;
     } else if (sezione === 'lingue') {
       this.lingueSelezionate = [...this.lingueSelezionateTmp];
@@ -188,24 +212,133 @@ export class TutorProfilePage implements OnInit {
     if (sezione === 'biografia') {
       this.biografiaTmp = this.biografia;
       this.isEditingBiografia = false;
-    } else if (sezione === 'lingue') {
-      this.isEditingLingue = false;
-    } else if (sezione === 'materie') {
-      this.isEditingMaterie = false;
-    } else if (sezione === 'disponibilita') {
+    } else if (sezione === 'lingue') this.isEditingLingue = false;
+    else if (sezione === 'materie') this.isEditingMaterie = false;
+    else if (sezione === 'disponibilita') {
       this.prezzoOrarioTmp = this.prezzoOrario;
       this.isEditingDisponibilita = false;
     }
   }
 
-  aggiungiDispensa() {
-    if (!this.nuovaDispensa.titolo || this.nuovaDispensa.prezzo === null)
-      return;
-    this.listaDispense.push({ ...this.nuovaDispensa });
-    this.nuovaDispensa = { titolo: '', descrizione: '', prezzo: null };
+  onAvatarSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => (this.avatarUrl = reader.result as string);
+      reader.readAsDataURL(file);
+    }
   }
 
-  selezionaCopertina() {}
-  selezionaFile() {}
-  selezionaAvatar() {}
+  onCopertinaSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () =>
+        (this.nuovaDispensa.copertinaUrl = reader.result as string);
+      reader.readAsDataURL(file);
+    }
+  }
+
+  onAnteprimaSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      if (file.type === 'application/pdf') {
+        const blobUrl = URL.createObjectURL(file);
+        this.nuovaDispensa.anteprimaUrl =
+          this.sanitizer.bypassSecurityTrustResourceUrl(blobUrl);
+        this.nuovaDispensa.isPdfPreview = true;
+        this.nuovaDispensa.haAnteprima = true;
+      } else if (file.type.startsWith('image/')) {
+        const reader = new FileReader();
+        reader.onload = () => {
+          this.nuovaDispensa.anteprimaUrl = reader.result as string;
+          this.nuovaDispensa.isPdfPreview = false;
+          this.nuovaDispensa.haAnteprima = true;
+        };
+        reader.readAsDataURL(file);
+      }
+    }
+  }
+
+  onFileCompletoSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      this.nuovaDispensa.fileCompleto = file;
+      this.nuovaDispensa.haFileCompleto = true;
+    }
+  }
+
+  aggiungiDispensa(
+    copertinaEl: HTMLInputElement,
+    anteprimaEl: HTMLInputElement,
+    fileCompletoEl: HTMLInputElement,
+  ) {
+    if (
+      !this.nuovaDispensa.titolo ||
+      this.nuovaDispensa.prezzo === null ||
+      !this.nuovaDispensa.haFileCompleto
+    ) {
+      alert(
+        'Compila i campi obbligatori (Titolo, Prezzo e Carica il File Completo).',
+      );
+      return;
+    }
+
+    // Creazione istanza pulita da salvare slegata dallo stato del form
+    const dispensaDaSalvare: Dispensa = {
+      titolo: this.nuovaDispensa.titolo,
+      descrizione: this.nuovaDispensa.descrizione,
+      prezzo: this.nuovaDispensa.prezzo,
+      copertinaUrl: this.nuovaDispensa.copertinaUrl,
+      anteprimaUrl: this.nuovaDispensa.anteprimaUrl,
+      isPdfPreview: this.nuovaDispensa.isPdfPreview,
+      fileCompleto: this.nuovaDispensa.fileCompleto,
+      haAnteprima: this.nuovaDispensa.haAnteprima,
+      haFileCompleto: this.nuovaDispensa.haFileCompleto,
+    };
+
+    this.listaDispense.push(dispensaDaSalvare);
+
+    // RESET MECCANICO DEL BROWSER (Sblocca l'evento onchange al caricamento successivo)
+    if (copertinaEl) copertinaEl.value = '';
+    if (anteprimaEl) anteprimaEl.value = '';
+    if (fileCompletoEl) fileCompletoEl.value = '';
+
+    // RESET COMPLETO DELLO STATO DATI DELLA NUOVA DISPENSA
+    this.nuovaDispensa = {
+      titolo: '',
+      descrizione: '',
+      prezzo: null,
+      copertinaUrl: '',
+      anteprimaUrl: '',
+      fileCompleto: null,
+      haAnteprima: false,
+      haFileCompleto: false,
+      isPdfPreview: false,
+    };
+  }
+
+  scaricaFileCompleto(dispensa: Dispensa) {
+    if (!dispensa.fileCompleto) {
+      alert('Nessun file scaricabile trovato.');
+      return;
+    }
+    const url = URL.createObjectURL(dispensa.fileCompleto);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = dispensa.fileCompleto.name;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  }
+
+  apriAnteprimaStudente(dispensa: Dispensa) {
+    this.dispensaInEvidenza = dispensa;
+    this.isViewingAnteprima = true;
+  }
+  chiudiAnteprimaStudente() {
+    this.isViewingAnteprima = false;
+    this.dispensaInEvidenza = null;
+  }
 }
