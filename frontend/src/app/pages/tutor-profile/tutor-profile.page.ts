bb8b45794c4fe4146c7e525a1400dbae238b8877
ruben-sanfilippo <rubensanfilippo2@gmail.com>
2026-05-26 12:40:32 +0200

    tutor profile aggiunta lato frontend

--- frontend/src/app/pages/tutor-profile/tutor-profile.page.ts
+++ frontend/src/app/pages/tutor-profile/tutor-profile.page.ts
@@ -1,36 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {
-  IonContent,
-  IonButton,
-  IonIcon,
-  IonInput,
-} from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 import { addIcons } from 'ionicons';
-import {
-  personOutline,
-  cameraOutline,
-  briefcaseOutline,
-  documentTextOutline,
-  globeOutline,
-  locationOutline,
-  calendarOutline,
-  cashOutline,
-  imageOutline,
-  cloudUploadOutline,
-  addCircleOutline,
-  documentOutline,
-  saveOutline,
-  star,
+import { 
+  personOutline, cameraOutline, star, briefcaseOutline, 
+  documentTextOutline, globeOutline, locationOutline, 
+  calendarOutline, saveOutline, libraryOutline, 
+  closeCircle, checkmarkDoneOutline, searchOutline, folderOpenOutline, 
+  createOutline, cashOutline, imageOutline, cloudUploadOutline, addCircleOutline, documentOutline
 } from 'ionicons/icons';
 
-// Interfaccia interna per tipizzare la struttura della dispensa
 interface Dispensa {
   titolo: string;
   descrizione: string;
   prezzo: number | null;
-  fileCopertina: any;
-  fileDispensa: any;
 }
 
 @Component({
@@ -38,114 +22,135 @@ interface Dispensa {
   templateUrl: './tutor-profile.page.html',
   styleUrls: ['./tutor-profile.page.scss'],
   standalone: true,
-  imports: [FormsModule, IonContent, IonButton, IonIcon, IonInput],
+  imports: [CommonModule, FormsModule, IonicModule]
 })
 export class TutorProfilePage implements OnInit {
-  // Dati del Tutor (In un caso reale verrebbero popolati da una chiamata GET al backend Express)
-  nome = 'Elena';
-  cognome = 'Rossi';
-  specializzazione = 'Senior Mathematics & Physics Tutor';
-  biografia =
-    'Appassionata nel rendere accessibili a chiunque i concetti fisici e matematici complessi...';
-  lingue = 'Italiano (Madrelingua), Inglese (Fluente)';
-  posizione = 'Milano, Italia (Disponibile Online)';
-  prezzoOrario = 45;
+  nome = 'Ruben';
+  cognome = 'Sanfilippo';
+  biografia = 'Ingegnere informatico con passione per l\'insegnamento delle materie scientifiche.';
+  mediaRecensioni = 4.8;
+  numeroRecensioni = 24;
+  prezzoOrario = 15;
 
-  // Dati statistici calcolati dal backend in base alle recensioni reali lasciate dagli studenti
-  mediaRecensioni = 4.9;
-  totaleRecensioni = 28;
+  materieSelezionate: string[] = ['Matematica', 'Fisica'];
+  lingueSelezionate: string[] = ['Italiano'];
 
-  // Array contenente le dispense caricate dal tutor
-  listaDispense: Dispensa[] = [
-    {
-      titolo: 'Meccanica Quantistica I - Appunti Completi',
-      descrizione:
-        "Raccolta di dispense ed esercizi svolti passo passo per superare l'esame scritto.",
-      prezzo: 15,
-      fileCopertina: null,
-      fileDispensa: null,
-    },
-  ];
+  // Variabili di stato "Editing" separate
+  isEditingBiografia = false;
+  isEditingLingue = false;
+  isEditingMaterie = false;
+  isEditingDisponibilita = false;
 
-  // Oggetto di appoggio per il form di caricamento della nuova dispensa
-  nuovaDispensa: Dispensa = {
-    titolo: '',
-    descrizione: '',
-    prezzo: null,
-    fileCopertina: null,
-    fileDispensa: null,
-  };
+  // Buffer Temporanei
+  biografiaTmp = '';
+  prezzoOrarioTmp = 0;
+  materieSelezionateTmp: string[] = [];
+  lingueSelezionateTmp: string[] = [];
+
+  // Pool per autocompletamento
+  materieDisponibili = ['Matematica', 'Fisica', 'Analisi Matematica 1', 'Informatica', 'Chimica', 'Geometria'];
+  materieFiltrate: string[] = [];
+  mostraListaMaterie = false;
+
+  lingueDisponibili = ['Italiano', 'Inglese', 'Spagnolo', 'Francese', 'Tedesco'];
+  lingueFiltrate: string[] = [];
+  mostraListaLingue = false;
+
+  nuovaDispensa: Dispensa = { titolo: '', descrizione: '', prezzo: null };
+  listaDispense: Dispensa[] = [];
 
   constructor() {
-    // Registrazione icone standalone per l'interfaccia di gestione del profilo
-    addIcons({
-      personOutline,
-      cameraOutline,
-      briefcaseOutline,
-      documentTextOutline,
-      globeOutline,
-      locationOutline,
-      calendarOutline,
-      cashOutline,
-      imageOutline,
-      cloudUploadOutline,
-      addCircleOutline,
-      documentOutline,
-      saveOutline,
-      star,
+    addIcons({ 
+      personOutline, cameraOutline, star, briefcaseOutline, 
+      documentTextOutline, globeOutline, locationOutline, 
+      calendarOutline, saveOutline, libraryOutline, 
+      closeCircle, checkmarkDoneOutline, searchOutline, folderOpenOutline, 
+      createOutline, cashOutline, imageOutline, cloudUploadOutline, addCircleOutline, documentOutline
     });
   }
 
-  ngOnInit() {}
-
-  selezionaCopertina() {
-    console.log(
-      "Logica per triggerare l'input file per l'immagine di copertina",
-    );
+  ngOnInit() {
+    this.biografiaTmp = this.biografia;
+    this.prezzoOrarioTmp = this.prezzoOrario;
+    this.materieFiltrate = [...this.materieDisponibili];
+    this.lingueFiltrate = [...this.lingueDisponibili];
   }
 
-  selezionaFile() {
-    console.log("Logica per triggerare l'input file per il PDF della dispensa");
+  attivaModificaLingue() {
+    this.lingueSelezionateTmp = [...this.lingueSelezionate];
+    this.isEditingLingue = true;
+  }
+
+  attivaModificaMaterie() {
+    this.materieSelezionateTmp = [...this.materieSelezionate];
+    this.isEditingMaterie = true;
+  }
+
+  filtraMaterie(ev: any) {
+    const val = ev.target.value.toLowerCase();
+    this.materieFiltrate = this.materieDisponibili.filter(m => m.toLowerCase().includes(val));
+  }
+
+  filtraLingue(ev: any) {
+    const val = ev.target.value.toLowerCase();
+    this.lingueFiltrate = this.lingueDisponibili.filter(l => l.toLowerCase().includes(val));
+  }
+
+  aggiungiMateria(m: string) {
+    if (!this.materieSelezionateTmp.includes(m)) this.materieSelezionateTmp.push(m);
+    this.mostraListaMaterie = false;
+  }
+
+  rimuoviMateria(m: string) {
+    this.materieSelezionateTmp = this.materieSelezionateTmp.filter(item => item !== m);
+  }
+
+  aggiungiLingua(l: string) {
+    if (!this.lingueSelezionateTmp.includes(l)) this.lingueSelezionateTmp.push(l);
+    this.mostraListaLingue = false;
+  }
+
+  rimuoviLingua(l: string) {
+    this.lingueSelezionateTmp = this.lingueSelezionateTmp.filter(item => item !== l);
+  }
+
+  salvaSezione(sezione: string) {
+    if (sezione === 'biografia') {
+      this.biografia = this.biografiaTmp;
+      this.isEditingBiografia = false;
+    } else if (sezione === 'lingue') {
+      this.lingueSelezionate = [...this.lingueSelezionateTmp];
+      this.isEditingLingue = false;
+    } else if (sezione === 'materie') {
+      this.materieSelezionate = [...this.materieSelezionateTmp];
+      this.isEditingMaterie = false;
+    } else if (sezione === 'disponibilita') {
+      this.prezzoOrario = this.prezzoOrarioTmp;
+      this.isEditingDisponibilita = false;
+    }
+  }
+
+  annullaModifica(sezione: string) {
+    if (sezione === 'biografia') {
+      this.biografiaTmp = this.biografia;
+      this.isEditingBiografia = false;
+    } else if (sezione === 'lingue') {
+      this.isEditingLingue = false;
+    } else if (sezione === 'materie') {
+      this.isEditingMaterie = false;
+    } else if (sezione === 'disponibilita') {
+      this.prezzoOrarioTmp = this.prezzoOrario;
+      this.isEditingDisponibilita = false;
+    }
   }
 
   aggiungiDispensa() {
-    if (
-      !this.nuovaDispensa.titolo ||
-      !this.nuovaDispensa.descrizione ||
-      !this.nuovaDispensa.prezzo
-    ) {
-      console.log(
-        'Validazione fallita: compilare tutti i dati della dispensa.',
-      );
-      return;
-    }
-
-    // Inserimento della dispensa nell'elenco visivo locale (usando lo spread operator)
+    if (!this.nuovaDispensa.titolo || this.nuovaDispensa.prezzo === null) return;
     this.listaDispense.push({ ...this.nuovaDispensa });
-
-    // Reset del form di inserimento
-    this.nuovaDispensa = {
-      titolo: '',
-      descrizione: '',
-      prezzo: null,
-      fileCopertina: null,
-      fileDispensa: null,
-    };
+    this.nuovaDispensa = { titolo: '', descrizione: '', prezzo: null };
   }
 
-  salvaProfilo() {
-    const payloadTutor = {
-      specializzazione: this.specializzazione,
-      biografia: this.biografia,
-      lingue: this.lingue,
-      posizione: this.posizione,
-      prezzoOrario: this.prezzoOrario,
-      dispense: this.listaDispense,
-    };
-
-    console.log(
-      'Payload completo pronto per essere inviato tramite chiamata PUT/PATCH a Express:',
-      payloadTutor,
-    );
-  }
+  selezionaCopertina() {}
+  selezionaFile() {}
+  selezionaAvatar() {}
 }
