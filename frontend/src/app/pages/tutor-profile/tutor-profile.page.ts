2c47e612823ba13e716ac828bbfac985afd7547b
ruben-sanfilippo <rubensanfilippo2@gmail.com>
2026-05-26 18:44:01 +0200

    piccola modifica

--- frontend/src/app/pages/tutor-profile/tutor-profile.page.ts
+++ frontend/src/app/pages/tutor-profile/tutor-profile.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
